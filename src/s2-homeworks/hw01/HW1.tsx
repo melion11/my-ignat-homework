ac3d7f3f9ef9cef7362e04540a2b83668002cc2c
melion11 <gphooenix@gmail.com>
2023-04-27 10:57:34 +0300

    add new styles of css

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -32,10 +32,10 @@ export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Iliya',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Hello, how are you?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -43,11 +43,11 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Nastya', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: "I'm fine, thank you", // можно менять
+        time: '22:01', // можно менять
     },
 }
 
